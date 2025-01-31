862e762c7ca6e8c87816b38589168f8d2004d68c
Barathan <barathan1799@gmail.com>
2025-01-31 13:43:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+# Step 1: Importing dependencies
 import streamlit as st
 import pandas as pd
 
+# Step 2: Setting app title and description
 st.set_page_config(page_title="BMW data", page_icon=":)")
 st.title("Analyzing the performance of different BMW models")
 st.write(
@@ -9,17 +11,20 @@ st.write(
     """
 )
 
-# Load the data from the Excel file. We're caching this so it doesn't reload every time the app
+# Step 3: Defining a function to load the data from the csv file. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
     df = pd.read_csv("PCA_BMW_dataset.csv")
     return df
 
-
+# Step 4: Executing the function to load data from csv file
 df = load_data()
 
-# Display the data as a table using `st.dataframe`.
+# Step 5: Renaming the first column title of the dataframe to "Model"
+df = df.rename(columns = {df.columns[0]: "Model"})
+
+# Step 6: Displaying the data as a table using `st.dataframe`.
 st.dataframe(
     df,
     use_container_width=True
